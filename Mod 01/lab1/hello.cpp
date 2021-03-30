c0ec89fdbd15207d71ab5629797ce6c346061b79
Chris <chris@ipushpixels.io>
2021-03-30 13:41:13 -0700

    Added lab1

---
+++ Mod 01/lab1/hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Added
+	int x = 5, y = 5;
+
+	// Orginal 
+	cout << "Hello world from linux!" << endl;
+
+	// Added
+	cout << "x = " << x << " and y = " << y << endl;
+	cout << "x + y = " << x + y << endl;
+
+	return 0;
+}
